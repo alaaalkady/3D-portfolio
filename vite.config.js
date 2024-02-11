05df615caf2c8375f550cc940b0c4f8ca78d3e18
alaaalkady <123353995+alaaalkady@users.noreply.github.com>
2024-02-11 11:50:36 +0200

    2nd version

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/3D-portfolio/'
 })
+
+
+/*// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/my-vite-app/'
+})*/
